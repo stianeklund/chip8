5d24208e378b3423e7a77f05e755569df28541d8
Stian Eklund <stian.eklund@gmail.com>
2016-11-24 17:10:57 +0100

    create new Cpu instance

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-mod cpu;
 
 use std::env;
 use std::fs;
 use std::io::Read;
 use cpu::Cpu;
 
+mod cpu;
+
 fn main() {
 
-    let file_name = env::args().nth(1).expect("env::args().nth(1) error");
+    let file_name = env::args().nth(1).expect("Missing input file");
     let mut file = fs::File::open(&file_name).unwrap();
     let mut file_buf = Vec::new();
     file.read_to_end(&mut file_buf).expect("Reading rom failed");
     // Create a new immutable binding
     let file_buf = file_buf;
+
+    let mut cpu = Cpu::new();
+
 }
