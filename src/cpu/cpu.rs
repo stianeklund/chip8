46c79b17cc34b7fc78b2e50e9ffc2aff6a564389
Stian Eklund <stian.eklund@gmail.com>
2016-11-24 15:17:06 +0100

    read rom to buf

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -1,7 +1,7 @@
 #[derive(Debug)
 pub struct Cpu {
     opcode: u16,
-    memory: [u8, 4096], // should perhaps be 4095?
+    memory: [u8, 4096],
     v: [u8; 16],
     i: isize,
     pc: usize,
@@ -10,19 +10,22 @@ pub struct Cpu {
     delay_timer: u8,
     sound_timer: u8,
 }
-
+// TODO: Implement keypad & display in SDL2
   impl Cpu {
       pub fn new() -> Cpu {
           let mut cpu = Cpu {
               opcode: 0,
               memory: [0; 4096],
               v: [0; 16],
-              i: 0x200, // start address for programs
-              pc: 0x200, // PC reset goes to same address
+              i: 0x200, // start address (except ETI 660)
+              pc: 0x200, // PC reset
               stack: [0; 16],
               sp: 0,
               delay_timer: 0,
-              sound_timer: 0
+              sound_timer: 0,
+              keypad: Keypad,
+              display: Display
+
           }
       }
   }
