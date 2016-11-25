710cd3c8bd8f6cbd4599ddfae3f2e8bc129e1c92
Stian Eklund <stian.eklund@gmail.com>
2016-11-25 12:19:38 +0100

    Add extra documentation on registers and shorten timers

--- src/cpu.rs
+++ src/cpu.rs
@@ -6,8 +6,8 @@ pub struct Cpu {
     pc: usize,
     stack: [u16; 16],
     sp: usize,
-    delay_timer: u8,
-    sound_timer: u8
+    dt: u8,
+    st: u8
     // keypad: Keypad,
     // display: Display
 }
@@ -18,13 +18,14 @@ impl Cpu {
         Cpu {
             opcode: 0,
             memory: [0; 4096],  // 0x000 (0) to 0xFFF (4095). 0x000 - 0x1FF for interpreter
-            v: [0; 16],         // 8-bit general purpose register, V0,V1 up to VE.
-            i: 0x200,           // Start address
+            v: [0; 16],         // 8-bit general purpose register, ()V0 - VE*).
+            i: 0x200,           // Index register (start address)
             pc: 0x200,          // Program Counter (start address is 0x200 on RST)
             stack: [0; 16],     // Interpreter returns to value when done with subroutine
             sp: 0,              // Stack pointer. Used to point to topmost level of the Stack
-            delay_timer: 0,
-            sound_timer: 0,
+            dt: 0,              // 8-bit Delay Timer
+            st: 0,              // 8-bit Sound timer
+            // * VF is a special register used to store the overflow bit
         }
     }
 }
