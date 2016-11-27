8dc8bed0881c8477f76e4b609f4f1e8b465bcd54
Stian Eklund <stian.eklund@gmail.com>
2016-11-27 21:43:02 +0100

    Don't loop if DEBUG = true

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,27 @@
 use std::env;
 use cpu::Cpu;
 mod cpu;
+const DEBUG: bool = true;
 
 fn main() {
     // Workaround
     let args: Vec<String> = env::args().collect();
 	  if args.len() != 2 {
-		    println!("[PATH_TO_ROM]");
+		    println!("[Path to rom]");
 		    return;
 	  }
 	  let bin = &args[1];
 
     let mut cpu = Cpu::new();
     cpu.load_bin(bin);
-
-    loop {
-    cpu.step();
-    cpu.update_timers();
+    // Lazy debugging..
+    if DEBUG {
+        cpu.step();
+        cpu.update_timers();
+    } else {
+        loop {
+            cpu.step();
+            cpu.update_timers();
+        }
     }
 }
